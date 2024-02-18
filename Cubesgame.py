d8fbac7965d7fab1dd1c23e53bb6d58f85d949cf
MaxmuscleMas <75904354+Maxasaran@users.noreply.github.com>
2024-02-19 00:25:33 +0300

    SimpleCubesGame

---
+++ Cubesgame.py
@@ -0,0 +1,16 @@
+### Бросаем кубики
+
+roll1 = [1,2,3,4,5,6]
+roll2 = [1,2,3,4,5,6]
+roll3 = [1,2,3,4,5,6]
+sum = 0
+count = 0
+for i in roll1:
+    for j in roll2:
+        for k in roll3:
+            sum = i+j+k
+            if sum > 7:
+                count += 1
+
+
+print(count)
